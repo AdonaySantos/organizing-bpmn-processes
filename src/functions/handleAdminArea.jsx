06e63434621a3032bc3f07190231fc3327036871
Adonay <adonay.rssantos@gmail.com>
2024-11-11 21:46:28 -0300

    Alteração na estrutura de chamada da função `handleAdminArea`

--- src/functions/handleAdminArea.jsx
+++ src/functions/handleAdminArea.jsx
@@ -1,3 +1,24 @@
-export const handleAdminArea = (navigate) => {
-  navigate("/administração");
+import axios from "axios";
+
+export const handleAdminArea = async (navigate) => {
+  try {
+    const token = localStorage.getItem("authToken");
+    if (!token) {
+      navigate("/"); // Redireciona para a página inicial se o token não estiver presente
+      return;
+    }
+  
+    const response = await axios.get(
+      "https://backend-southstar.onrender.com/administracao",
+      {
+        headers: {
+          Authorization: token, // Envia o token JWT no cabeçalho
+        },
+      }
+    );
+
+    navigate("/administração");
+  } catch (error) {
+    navigate("/"); // Redireciona para a página inicial em caso de erro ou falta de permissão
+  }
 };
